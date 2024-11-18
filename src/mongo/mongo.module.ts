1a0e567159cbb34060ef40b2cdee3cbb3dbf8a97
Roman Matveev <r.matveev@socrat.marketing>
2024-11-18 18:37:00 +1000

    19.11.2024 -- [nongo/module]: Регистрация модели ChatModel в mongo.module.ts

--- src/mongo/mongo.module.ts
+++ src/mongo/mongo.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongoService } from './mongo.service';
 import {MongooseModule} from "@nestjs/mongoose";
 import {MongoConfig} from "./mongo.config";
+import {ChatModel, ChatSchema} from "./models/chat.model";
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import {MongoConfig} from "./mongo.config";
           imports: [MongoModule],
           inject: [MongoConfig],
       }),
+      MongooseModule.forFeature([{name: ChatModel.name, schema: ChatSchema}])
   ],
   exports: [MongoConfig],
   providers: [MongoService, MongoConfig]
--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {ChatDocument, ChatModel} from "./models/chat.model";
+import {Model} from "mongoose";
 
 @Injectable()
-export class MongoService {}
+export class MongoService {
+    constructor(@InjectModel(ChatModel.name) private readonly  chatModel: Model<ChatDocument>) {
+    }
+
+    /*async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+        return await new this.chatModel({
+            ...createTodoDto,
+            createdAt: new Date(),
+        }).save();
+    }
+
+
+    async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+        return await this.chatModel.findByIdAndUpdate(id, updateTodoDto).exec();
+    }
+
+
+
+    async delete(id: string): Promise<Todo> {
+        return await this.chatModel.findByIdAndDelete(id).exec();
+    }*/
+}
