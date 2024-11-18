c9d086c7fc2dd8103310ba454ca1f26cf0b258a9
Roman Matveev <r.matveev@socrat.marketing>
2024-11-18 10:54:41 +1000

    19.11.2024 -- React клиент. Прокрута чата вниз при получении нового сообщения.

--- client/src/views/ChatMessageView.jsx
+++ client/src/views/ChatMessageView.jsx
@@ -1,9 +1,40 @@
 import ChatBadgeView from "./ChatBadgeView";
+import {useEffect, useRef} from "react";
 
 const ChatMessageView = ({messages, currentUser}) => {
+    const scrollChat = useRef(null);
+
+    function scrollUp() {
+        if ((scrollChat?.current?.scrollTop === 0) && (messages?.length > 8)){
+            //page_++;
+            loadMore();
+        }
+    }
+
+    useEffect(() => {
+        if (messages?.length > 5) {
+            scrollToBottom();
+        }
+    }, [messages]);
+
+    const scrollToBottom = () => {
+        scrollChat?.current?.scrollTo(0,scrollChat?.current.scrollHeight);
+    }
+
+
+    function loadMore(isDropPage = false) {
+    }
+
+    useEffect(() => {
+        scrollChat?.current.addEventListener("scroll", scrollUp);
+        return () => {
+            scrollChat?.current?.removeEventListener("scroll", scrollUp);
+        }
+    }, []);
+
     return (
         <>
-            <div className="d-flex flex-column flex-grow-1 overflow-scroll card border border-dark p-3">
+            <div ref={scrollChat} className="d-flex flex-column flex-grow-1 overflow-scroll card border border-dark p-3">
                 {messages.length > 0 ?
                     messages.map((item, k) => <div key={k}><ChatBadgeView item={item} isMyMsg={item?.user?.user === currentUser?.userId}/></div>)
                     : <div className="d-flex justify-content-center text-danger">
