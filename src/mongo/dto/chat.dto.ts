2898c7d951c4fd229459b5dad4c4fb9472e2ad84
Roman Matveev <r.matveev@socrat.marketing>
2024-11-18 18:49:22 +1000

    19.11.2024 -- [nongo/module]: класс mongo.service.ts -- описание основных crud операций.

---
+++ src/mongo/dto/chat.dto.ts
@@ -0,0 +1,8 @@
+
+export class ChatDto {
+    who: string;
+    executorId: string;
+    customerId: string;
+    room: string; //room-uuid
+    message: string;
+}
--- src/mongo/mongo.service.ts
+++ src/mongo/mongo.service.ts
@@ -2,27 +2,38 @@ import { Injectable } from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {ChatDocument, ChatModel} from "./models/chat.model";
 import {Model} from "mongoose";
+import {ChatDto} from "./dto/chat.dto";
 
 @Injectable()
 export class MongoService {
     constructor(@InjectModel(ChatModel.name) private readonly  chatModel: Model<ChatDocument>) {
     }
 
-    /*async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    /**
+     * Создать сообщение чата
+     * @param chatDto ChatDto
+     */
+    async create(chatDto: ChatDto) {
         return await new this.chatModel({
-            ...createTodoDto,
+            ...chatDto,
             createdAt: new Date(),
         }).save();
     }
 
-
-    async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
-        return await this.chatModel.findByIdAndUpdate(id, updateTodoDto).exec();
+    /**
+     * Обновить сообщение чата
+     * @param id string
+     * @param chatDto ChatDto
+     */
+    async update(id: string, chatDto: ChatDto) {
+        return await this.chatModel.findByIdAndUpdate(id, chatDto).exec();
     }
 
-
-
-    async delete(id: string): Promise<Todo> {
+    /**
+     * Удалить сообщение чата
+     * @param id string
+     */
+    async delete(id: string): Promise<ChatModel> {
         return await this.chatModel.findByIdAndDelete(id).exec();
-    }*/
+    }
 }
