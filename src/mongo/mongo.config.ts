c85dbc44caefe490f4cbb45848207a499b5a1671
Roman Matveev <r.matveev@socrat.marketing>
2024-11-18 15:15:04 +1000

    19.11.2024 -- [nongo/module]: Создание сценариев инициализации пользователя и базы данных для mongodb

--- src/mongo/mongo.config.ts
+++ src/mongo/mongo.config.ts
@@ -7,11 +7,11 @@ export class MongoConfig {
     }
 
     public getMongoUri() {
-        const login = this.configService.getOrThrow('MONGO_LOGIN') ?? '';
-        const password = this.configService.getOrThrow('MONGO_PASSWORD') ?? '';
+        const login = this.configService.getOrThrow('MONGO_USER_LOGIN') ?? '';
+        const password = this.configService.getOrThrow('MONGO_USER_PASSWORD') ?? '';
         const host = this.configService.getOrThrow('MONGO_HOST') ?? '';
         const database = this.configService.getOrThrow('MONGO_DB_NAME') ?? '';
         console.warn(`mongodb://${login}:${password}@${host}:27017/${database}`)
-        return `mongodb://${login}:${password}@${host}:27017/`
+        return `mongodb://${login}:${password}@${host}:27017/${database}`
     }
 }
