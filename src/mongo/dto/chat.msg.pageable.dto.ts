652a691f2571c85527a6d14fc93f4d0c16bfead5
Roman Matveev <r.matveev@socrat.marketing>
2024-11-21 15:27:25 +1000

    21.11.2024 -- [nongo/module]: класс chat.msg.pageable.dto.ts -- подкорректированы поля page и size

--- src/mongo/dto/chat.msg.pageable.dto.ts
+++ src/mongo/dto/chat.msg.pageable.dto.ts
@@ -1,4 +1,4 @@
-import {IsInt, IsNotEmpty, IsString, Min} from "class-validator";
+import {IsInt, IsNotEmpty, isNumber, IsString, Min} from "class-validator";
 import {ApiValidateMessages} from "../../common/api.validate-messages";
 
 export class ChatMsgPageableDto {
@@ -8,14 +8,6 @@ export class ChatMsgPageableDto {
     @IsString({message: ApiValidateMessages.stringValidationMessage})
     @IsNotEmpty({message: ApiValidateMessages.notEmptyValidationMessage})
     userId: string;
-    @IsInt({message: ApiValidateMessages.intValidationMessage})
-    @Min(0, {
-        message: ApiValidateMessages.minValidationMessage,
-    })
-    page: number;
-    @IsInt({message: ApiValidateMessages.intValidationMessage})
-    @Min(10, {
-        message: ApiValidateMessages.minValidationMessage,
-    })
-    size: number;
+    page: number = 0;
+    size: number = 20;
 }
